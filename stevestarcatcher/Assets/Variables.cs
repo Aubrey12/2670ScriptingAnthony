ad99022fa917ecd2210c2d3705cbe1956517d9ed
Aubrey12 <aubrey1223@hotmail.com>
2016-09-15 15:55:35 -0600

    Update .gitignore
    Added assignment for variables

---
+++ stevestarcatcher/Assets/Variables.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variables : MonoBehaviour {
+
+	public float apple = 5;
+
+	// Use this for initialization
+	void Start () {
+			
+		apple = 3;
+		print ("this is a gooood apple");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
