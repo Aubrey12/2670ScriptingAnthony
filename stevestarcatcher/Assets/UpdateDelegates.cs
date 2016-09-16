8271e93a74af900d42f43267fc2601fb679d21a4
Aubrey12 <aubrey1223@hotmail.com>
2016-09-16 17:10:21 -0600

    Updated Delegates script

---
+++ stevestarcatcher/Assets/UpdateDelegates.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UpdateDelegates : MonoBehaviour {
+
+	int health = 250;
+
+	Action DisplayHealth;
+	Action KillThePlayer;
+	Action EndTheGame;
+
+	//assigned function to the delegate
+	void Start () 
+	{
+		DisplayHealth = DisplayHealthHandler;
+
+		void KillThePlayerHandler ()
+		{
+		print("you died. you didn't even try.");
+		EndTheGame = null;
+		}
+			
+		void DisplayHealthHandler() //unassigned all functions from display
+		{
+		print ("health is good");
+		DisplayHealtHandler = null;
+		}
+
+		KillThePlayer = KillThePlayerHandler;
+			void KillThePlayerHandler()
+			{
+				DisplayHealth--;
+				if (DisplayHealth < 0)
+					print (health);
+				if (health < 0)
+					KillThePlayer = null;
+			}
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (DisplayHealth != null)
+			DisplayHealthHandler ();
+	
+		if(KillThePlayer != null)
+			KillThePlayer();
+		
+		if(EndTheGame != null)
+			EndTheGame;
+	}
+}
