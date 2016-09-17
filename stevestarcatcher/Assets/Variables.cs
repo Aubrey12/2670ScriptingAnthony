9f44442ef5736b122fda6b6ab74460cfce2600d3
Aubrey <aubrey1223@hotmail.com>
2016-09-17 14:02:17 -0600

    Assignment for Conventions and Syntax

--- stevestarcatcher/Assets/Variables.cs
+++ stevestarcatcher/Assets/Variables.cs
@@ -15,8 +15,17 @@ public class Variables : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//Variables are basically used to contain information. If you were wanting to write a script to show a ball rolling you would create variables
-		//for the ball speed. Variables are always must be created in every script because without an ojbect there can be no action. We can't use functions
-		//without having a variable. 
-	}
+		/*Variables are basically used to contain information. If you were wanting to write a script to show a ball rolling you would create variables
+		for the ball speed. Variables are always must be created in every script because without an ojbect there can be no action. We can't use functions
+		without having a variable. 
+
+		Conventions and Syntax- Unity uses functions, classes, code brackets, comments to make up their conventions. At the top using UnityEngine
+		and System.Collection are name sakes that allow us to use code that has been pre-made in this class. Public class delcares the class and class name.
+		We use functions to show the actions taking place within the code. For example Start function begins at the start of the scene and the update 
+		function runs once per frame. Brackets are used to show when code is starting and ending. Comments can be made for a line of code by using //
+		If you want to comment several lines use multi-code /* and reverse for  to end the comment.  
+		*/
+	
+
+}
 }
