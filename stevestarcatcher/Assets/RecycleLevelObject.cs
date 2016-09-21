0ac925c3823e57707043e4f52cb6ce092ed3bd14
Unknown <aubrey1223@hotmail.com>
2016-09-21 14:10:27 -0600

    in class recycler

--- stevestarcatcher/Assets/RecycleLevelObject.cs
+++ stevestarcatcher/Assets/RecycleLevelObject.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
+//gives us access to lists
 
 public class RecycleLevelObject : MonoBehaviour {
 
 	private Vector3 newLocation;
+    public List<Recycler> recycleableList;
+    private int i = 0;
 
-	void OnTriggerEnter()
+    void Start()
+    {
+        recycleableList = new List<Recycler>();
+        Recycler.RecycleAction += RecycleActionHandler;
+    }
+
+    private void RecycleActionHandler(Recycler _r)
+    {
+        recycleableList.Add(_r);
+    }
+
+    void OnTriggerEnter()
 	{
 		newLocation.x = StaticVars.nextSectionPosition;
-		transform.position = newLocation;
+		recycleableList[i].cube.position = newLocation;
 		StaticVars.nextSectionPosition += StaticVars.distance;
+        if (i < recycleableList.Count-1)
+            i++;
 
 	}
 
