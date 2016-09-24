c0c4e2c0399f6fedffa3823132a2ba58d1581b21
Aubrey12 <aubrey1223@hotmail.com>
2016-09-24 13:53:49 -0600

    Assignment for Access Modifiers and Scope

--- stevestarcatcher/Assets/Variables.cs
+++ stevestarcatcher/Assets/Variables.cs
@@ -1,38 +1,29 @@
-﻿/*using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class Variables : MonoBehaviour {
 
-	public float apple = 5;
-	public float maxApples = 10;
-	public float minApples = 0;
+	public int apple = 5;
+
 
 	// Use this for initialization
 	void Start () {
+		int i = 0;
+		for (i = 0; apple > i; ++i);
 
-		for (i = 0; apple > 7; ++1);
-		print ("I want another apple please!")
-	
 	}
 
-	/*Loops are as way to repeat code. The For Loop has as variable that will be initialized iterated, a conditional and an increment or decrement. 
-	 	The While loop will continue to run until the conditional is true. Do While loops test the condtional at end of body. For each loop is useful
-		arrays it will run through each option.
 
+	void Update () {
 
+		if (apple > 0)
+			print("apples!!!");
 
-		Variables are basically used to contain information. If you were wanting to write a script to show a ball rolling you would create variables
-		for the ball speed. Variables are always must be created in every script because without an ojbect there can be no action. We can't use functions
-		without having a variable. 
+	/*Access Modifiers can be stated by using "public" or "private" before a variable. When set to private a variable can only be accessed within that
+	class. If set to public it can be accessed in other classes and in the inspector. Scope is a class's parameters which are set using the curly 
+	braces. If the access modifier is not specified the variable will default to private automatically. You are able to change the variable value in the
+	inspector but it will be over ridden by the value stated within code. 
 
-		Conventions and Syntax- Unity uses functions, classes, code brackets, comments to make up their conventions. At the top using UnityEngine
-		and System.Collection are name sakes that allow us to use code that has been pre-made in this class. Public class delcares the class and class name.
-		We use functions to show the actions taking place within the code. For example Start function begins at the start of the scene and the update 
-		function runs once per frame. Brackets are used to show when code is starting and ending. Comments can be made for a line of code by using //
-		If you want to comment several lines use multi-code /* and reverse for  to end the comment.  
-
-
-	
-
+*/
+	}
 }
-} */
