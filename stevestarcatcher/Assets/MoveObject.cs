67d5c9f68b63670e77e0a69d15880cc2d9481926
Aubrey <aubrey1223@hotmail.com>
2016-09-29 14:12:25 -0600

    fdlaskjlaskdjfl;kjsdf

--- stevestarcatcher/Assets/MoveObject.cs
+++ stevestarcatcher/Assets/MoveObject.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MoveObject : MonoBehaviour {
 
     //this is the character controller component
-   private CharacterController myCC;
+   public CharacterController myCC;
     //temp var of datatype vector3 to move the character
     private Vector3 tempPos;
     //speed of the remp in x
@@ -17,28 +17,11 @@ public class MoveObject : MonoBehaviour {
 	public int slideDuration = 100;
 	public float slideTime = 0.01f;
 
-	//Coroutine for sliding Character
-	IEnumerator Slide ()
-	{
-		int durationTemp = slideDuration;
-		float speedTemp = speed;
-		speed += speed;
-		//while loop runs /while/ the slideDuration >0
-		while (slideDuration > 0) {
-			//decrement the slideDuration
-			slideDuration--;
-			yield return new WaitForSeconds (slideTime);
-			print ("sliding");
-		}
-		speed = speedTemp;
-		slideDuration = durationTemp;
+	void Start (){
 
+		myCC = GetComponent<CharacterController>();
+		
 	}
-
-    void Start() {
-        myCC = GetComponent<CharacterController>();
-
-    }      
 
 	// Update is called once per frame
 	void Update () {
@@ -51,24 +34,17 @@ public class MoveObject : MonoBehaviour {
             tempPos.y = jumpSpeed;
         }
 
-		//start sliding
-		if (Input.GetKey(KeyCode.RightArrow) && Input.GetKeyDown (KeyCode.S)) 
-		{
-			StartCoroutine(Slide());
-		}
-		if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKeyDown (KeyCode.S)) 
-		{
-			StartCoroutine(Slide());
-		}
-
 		if(myCC.isGrounded)
 		{
 			jumpCount = 0;
 		}
-
+		//adding the gravity var to the y position of the tempPos var
+	
         tempPos.y -= gravity;
+		//adding the speed var to the tempPos var x value with the right and left arrow keys
 
         tempPos.x = speed*Input.GetAxis("Horizontal");
+		//Moves the charactor controller at an even pace (deltaTime)
         myCC.Move(tempPos * Time.deltaTime);
 
 	
