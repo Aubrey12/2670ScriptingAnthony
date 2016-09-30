0f116608993e1fda2940b960166d3a2b5fc86317
Unknown <aubrey1223@hotmail.com>
2016-09-30 15:21:33 -0600

    RECYCLER IS WORKING! CHARACTER NOT FALLING THROUGH!

---
+++ stevestarcatcher/Assets/SendToRecycler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SendToRecycler : MonoBehaviour
+{
+
+    bool canRecycle;
+    public static Action<SendToRecycler> SendThis;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (canRecycle && SendThis != null)
+            SendThis(this);
+
+    }
+
+}
